31dd9efe7ea1db3067721fd623bf1c4b12820849
Mathew Parker <mathew.louis.parker@gmail.com>
2025-06-28 16:09:54 +0100

    Switch to decap

---
+++ node_modules/decap-cms-backend-aws-cognito-github-proxy/dist/esm/index.js
@@ -0,0 +1,9 @@
+import { API } from 'decap-cms-backend-github';
+import AwsCognitoGitHubProxyBackend from './implementation';
+import AuthenticationPage from './AuthenticationPage';
+export const DecapCmsBackendAwsCognitoGithubProxy = {
+  AwsCognitoGitHubProxyBackend,
+  API,
+  AuthenticationPage
+};
+export { AwsCognitoGitHubProxyBackend, API, AuthenticationPage };
---
+++ node_modules/decap-cms-backend-git-gateway/dist/esm/index.js
@@ -0,0 +1,7 @@
+import GitGatewayBackend from './implementation';
+import AuthenticationPage from './AuthenticationPage';
+export const DecapCmsBackendGitGateway = {
+  GitGatewayBackend,
+  AuthenticationPage
+};
+export { GitGatewayBackend, AuthenticationPage };
